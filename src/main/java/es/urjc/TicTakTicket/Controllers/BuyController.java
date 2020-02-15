cac5a8a5c899cf3da05ce295a8db5f4e95bd808e
Carlos <carlete96@hotmail.com>
2020-02-15 20:16:44 +0100

    Añadida funcionalidad de compra
    Ahora se pueden comprar tickets (falta solucionar problemas en las tablas)

--- src/main/java/es/urjc/TicTakTicket/Controllers/BuyController.java
+++ src/main/java/es/urjc/TicTakTicket/Controllers/BuyController.java
@@ -1,15 +1,20 @@
 package es.urjc.TicTakTicket.Controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import es.urjc.TicTakTicket.Entities.Event;
 import es.urjc.TicTakTicket.Entities.EventRepository;
+import es.urjc.TicTakTicket.Entities.Order;
+import es.urjc.TicTakTicket.Entities.OrderRepository;
 import es.urjc.TicTakTicket.Entities.Payment;
 import es.urjc.TicTakTicket.Entities.PaymentRepository;
 import es.urjc.TicTakTicket.Entities.Ticket;
@@ -25,6 +30,9 @@ public class BuyController {
 	
 	@Autowired
 	private TicketRepository ticketR;
+	
+	@Autowired
+	private OrderRepository orderR;
 	
 	@Autowired
 	private PaymentRepository paymentR;
@@ -48,6 +56,68 @@ public class BuyController {
 		model.addAttribute("page_title", "Compra");
 		
 		return "buy_template";
+	}
+	
+	@PostMapping("/pay")
+	public String Submit(Model model, @RequestParam(required = false) String selectedCard,
+			@RequestParam String person_0, @RequestParam String price_0, @RequestParam String tickettype_0,
+			@RequestParam String person_1, @RequestParam String price_1, @RequestParam String tickettype_1,
+			@RequestParam String person_2, @RequestParam String price_2, @RequestParam String tickettype_2,
+			@RequestParam String person_3, @RequestParam String price_3, @RequestParam String tickettype_3,
+			@RequestParam String person_4, @RequestParam String price_4, @RequestParam String tickettype_4
+			) {
+		
+		User currentUser = userR.findById("default").get();
+		
+		if(selectedCard != "" && selectedCard != null) {
+			
+			// Imaginate que te cobro aqui de la tarjeta
+			
+			List<String> names = new ArrayList<String>();
+			List<Ticket> tickets = new ArrayList<Ticket>();
+			
+			if (person_0 != "") {
+				names.add(person_0);
+				Ticket ticket = ticketR.findById(Integer.parseInt(tickettype_0)).get();
+				tickets.add(ticket);
+			}
+			
+			if (person_1 != "") {
+				names.add(person_1);
+				Ticket ticket = ticketR.findById(Integer.parseInt(tickettype_1)).get();
+				tickets.add(ticket);
+			}
+			
+			if (person_2 != "") {
+				names.add(person_2);
+				Ticket ticket = ticketR.findById(Integer.parseInt(tickettype_2)).get();
+				tickets.add(ticket);
+			}
+			
+			if (person_3 != "") {
+				names.add(person_3);
+				Ticket ticket = ticketR.findById(Integer.parseInt(tickettype_3)).get();
+				tickets.add(ticket);
+			}
+			
+			if (person_4 != "") {
+				names.add(person_4);
+				Ticket ticket = ticketR.findById(Integer.parseInt(tickettype_4)).get();
+				tickets.add(ticket);
+			}
+			
+			Order order = new Order(names, tickets, currentUser);
+			
+			orderR.save(order);
+			
+			return "redirect:/events";
+			
+		} else {
+			
+			return "redirect:/user";
+			
+		}
+		
 	}
 	
 }
