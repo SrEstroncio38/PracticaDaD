f92ac234ebc79256af224c38772856da27ebdb7b
Carlos <carlete96@hotmail.com>
2020-04-28 20:15:19 +0200

    Prueba de sesión distribuida
    Intento de implementar la sesión distribuida

--- PracticaDaD/src/main/java/es/urjc/TicTakTicket/TicTakTicketApplication.java
+++ PracticaDaD/src/main/java/es/urjc/TicTakTicket/TicTakTicketApplication.java
@@ -1,10 +1,18 @@
 package es.urjc.TicTakTicket;
 
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class TicTakTicketApplication {
 
 	public static void main(String[] args) {
@@ -12,5 +20,13 @@ public class TicTakTicketApplication {
 		
 	}
 	
+	@Bean
+	public Config config() {
+		Config config = new Config();
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+		joinConfig.getMulticastConfig().setEnabled(false);
+		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+		return config;
+	}
 
 }
