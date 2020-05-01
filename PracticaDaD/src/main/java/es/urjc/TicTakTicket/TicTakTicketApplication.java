4810adf76405c266229f7bb7f336eff6f68af48f
David Fontela <d.fontela.1998@gmail.com>
2020-05-02 01:26:04 +0200

    Docker-compose final
    Funciona la aplicacion completa con docker-compose aunque tiene errores en la cache local de Java

--- PracticaDaD/src/main/java/es/urjc/TicTakTicket/TicTakTicketApplication.java
+++ PracticaDaD/src/main/java/es/urjc/TicTakTicket/TicTakTicketApplication.java
@@ -28,8 +28,8 @@ public class TicTakTicketApplication {
 	public Config config() {
 		Config config = new Config();
 		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
-		joinConfig.getMulticastConfig().setEnabled(false);
-		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+		joinConfig.getMulticastConfig().setEnabled(true);
+		//joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
 		return config;
 	}
 	
