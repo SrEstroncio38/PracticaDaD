d33b941956c4d18fead4c19f3558a61bcde813df
Carlos <carlete96@hotmail.com>
2020-05-02 20:20:52 +0200

    Arreglo en la sesión distribuida 3

--- PracticaDaD/src/main/java/es/urjc/TicTakTicket/TicTakTicketApplication.java
+++ PracticaDaD/src/main/java/es/urjc/TicTakTicket/TicTakTicketApplication.java
@@ -6,31 +6,15 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.session.data.redis.RedisOperationsSessionRepository;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableCaching
 @SpringBootApplication
-@EnableRedisHttpSession
 public class TicTakTicketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicTakTicketApplication.class, args);
 		
 	}
-	
-	@Bean
-	  public JedisConnectionFactory redisConnectionFactory() {
-	    return new JedisConnectionFactory();
-	  }
-	
-    @Bean
-    public RedisOperationsSessionRepository sessionRepository( RedisConnectionFactory factory) {
-        RedisOperationsSessionRepository sessionRepository = new RedisOperationsSessionRepository(factory);
-        return sessionRepository;
-    }
 	
 	@Bean
 	public CacheManager cacheManager() {
