d3f456890671afefc99f6787cc66a3d82c7a5732
Carlos <carlete96@hotmail.com>
2020-05-02 19:50:12 +0200

    Arreglo en la sesión distribuida

--- PracticaDaD/src/main/java/es/urjc/TicTakTicket/TicTakTicketApplication.java
+++ PracticaDaD/src/main/java/es/urjc/TicTakTicket/TicTakTicketApplication.java
@@ -1,12 +1,15 @@
 package es.urjc.TicTakTicket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableCaching
@@ -23,6 +26,16 @@ public class TicTakTicketApplication {
 	  public JedisConnectionFactory redisConnectionFactory() {
 	    return new JedisConnectionFactory();
 	  }
+	
+	@Value("${spring.session.timeout}")
+    private Integer maxInactiveIntervalInSeconds;
+	
+    @Bean
+    public RedisOperationsSessionRepository sessionRepository( RedisConnectionFactory factory) {
+        RedisOperationsSessionRepository sessionRepository = new RedisOperationsSessionRepository(factory);
+        sessionRepository.setDefaultMaxInactiveInterval(maxInactiveIntervalInSeconds);
+        return sessionRepository;
+    }
 	
 	@Bean
 	public CacheManager cacheManager() {
